c128a5c189359a2fdd07ac7c8526ad691ab6dbae
kimanifinplus <kimani@finplusgroup.com>
2023-09-06 20:35:53 +0300

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # Api definition
+  # Rails will automatically map the namespace to a directory matching the name
+  # under the controllers folder, in our case the api/` directory.
+  namespace :api, defaults: { format: :json } do
+    # We are going to list our resources here
+  end
 
 end
