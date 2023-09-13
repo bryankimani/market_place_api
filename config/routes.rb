61e63b1fac16cd695363a55cdff81bade70d65e2
kimanifinplus <kimani@finplusgroup.com>
2023-09-13 20:20:21 +0300

    Setup tokens controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       # We are going to list our resources here
       resources :users, only: [:show, :create, :update, :destroy]
+      resources :tokens, only: [:create]
     end
   end
 
