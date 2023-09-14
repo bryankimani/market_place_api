bd6928f8392616f7f802a656bdcf22b8590f01e4
kimanifinplus <kimani@finplusgroup.com>
2023-09-14 08:51:40 +0300

    Finishes modeling the product model with user associations

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
       # We are going to list our resources here
       resources :users, only: [:show, :create, :update, :destroy]
       resources :tokens, only: [:create]
+      resources :products, only: [:show, :index]
     end
   end
 
